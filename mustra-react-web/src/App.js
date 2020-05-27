84f2620e3b5bddabe4aa215e5292e92a97af596f
winterlood <king199777@gmail.com>
2020-05-27 18:45:55 +0900

    Update project template

--- mustra-react-web/src/App.js
+++ mustra-react-web/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState, useEffect } from "react";
+import AcUnitIcon from "@material-ui/icons/AcUnit";
+import ReplyModule from "module/ReplyModule/ReplyModule";
+const App = () => {
+    return (
+        <div className="App">
+            hello react
+            <ReplyModule />
+            <AcUnitIcon />
+        </div>
+    );
+};
 
 export default App;
---
+++ mustra-react-web/src/module/ReplyModule/ReplyModule.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const ReplyModule = () => {
+    return <div className="ReplyModule">reply Module</div>;
+};
+
+export default ReplyModule;
