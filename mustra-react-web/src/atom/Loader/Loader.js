d36d1818f44977ef2714011d38299f09e400125a
winterlood <king199777@gmail.com>
2020-05-28 15:13:45 +0900

    예측모듈 스타일 완료

---
+++ mustra-react-web/src/atom/Loader/Loader.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import "./Loader.css";
+const Loader = () => {
+    return (
+        <div className="Loader">
+            <div class="loader">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    );
+};
+
+export default Loader;
